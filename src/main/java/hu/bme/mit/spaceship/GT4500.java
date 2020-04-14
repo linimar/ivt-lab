2bbe94674e15b755f7a7d03f6fb30e0fb5943855
Linninger Martin <linninger.martin@gmail.com>
2020-04-14 17:06:59 +0200

    Feladat 3

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,8 +77,14 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+         if(!primaryTorpedoStore.isEmpty()){
+          firingSuccess = firingSuccess || primaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = true;
+        }
+        if(!secondaryTorpedoStore.isEmpty()){
+          firingSuccess = firingSuccess || secondaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = false;
+        }
 
         break;
     }
