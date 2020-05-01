a348151c61680044f016d14a9379664f0067814a
Linninger Martin <linninger.martin@gmail.com>
2020-05-01 19:41:21 +0200

    Make random generator private

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,7 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-  Random generator = new Random();
+ private  Random generator = new Random();
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
